a550b2c442384eedf0a582331fc89bf0d75c621c
geekygreta <greta.gustava@gmail.com>
2014-11-29 15:09:31 -0800

    Yet another initial commit

---
+++ src/org/translifeline/cupcakehotline/CupcakehotlineServlet.java
@@ -0,0 +1,13 @@
+package org.translifeline.cupcakehotline;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CupcakehotlineServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println(req.toString());
+	}
+}
